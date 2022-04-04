54be11d718f23bd2e831ae0071000e8729f56e93
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-04 19:02:10 +0200

    First Chapter Console
    a reminder of the basics of the basics:
    operators
    arithmetic
    Console comand

---
+++ CreatingConsoleApp/Welcome1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace CreatingConsoleApp
+{
+    internal class Welcome1
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            string person = "Paul";
+            Console.WriteLine($"Welcome to C# course, {person}.");
+
+            //NumberAddition();
+            OperatorsAndIF();
+
+        }
+
+
+
+
+
+
+
+        static void NumberAddition()
+        {
+            int number1;
+            int number2;
+            int sum;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Eneter first integer: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            number1 = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Eneter secound integer: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            number2 = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+            sum = number1 + number2;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write($"Sum = {sum} \n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        static void OperatorsAndIF()
+        {
+            int number1;
+            int number2;
+            
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Eneter first integer: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            number1 = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Eneter secound integer: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            number2 = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+            
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            if (number1 == number2) Console.WriteLine($"{number1} == {number2}");
+            if (number1 != number2) Console.WriteLine($"{number1} != {number2}");
+            if (number1 < number2) Console.WriteLine($"{number1} < {number2}");
+            if (number1 > number2) Console.WriteLine($"{number1} > {number2}");
+            if (number1 <= number2) Console.WriteLine($"{number1} <= {number2}");
+            if (number1 >= number2) Console.WriteLine($"{number1} >= {number2}");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+    }
+}
