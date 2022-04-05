d181be1ce230417a6b5121a33b1568584678ce5d
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-05 22:12:10 +0200

    Class Basic
    Course about set and get methods

---
+++ ConsoleAppClassBasic/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppClassBasic
+{
+    internal class Account
+    {
+        private string name;
+
+        public string Name 
+        { 
+            get 
+            { 
+                return name; 
+            }
+            set
+            {
+                name = value;
+            }
+        }   
+
+
+
+
+    }
+}
---
+++ ConsoleAppClassBasic/AccountTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppClassBasic
+{
+    internal class AccountTest
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+
+            Console.WriteLine($"Initial name is : {myAccount.Name}");
+
+            Console.Write("Enter the name: ");
+            string theName = Console.ReadLine();
+            myAccount.Name = theName;
+
+            Console.WriteLine($"myAccount name is : {myAccount.Name}");
+        }
+    }
+}
+                                                                                                                          
