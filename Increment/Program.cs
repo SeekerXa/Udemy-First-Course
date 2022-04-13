2165b4ebbdd681bee8e06e48656f636e7e3c8f80
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-13 16:02:24 +0200

    increment
    increment numbers

---
+++ Increment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 5;
+            Console.WriteLine($"c before postincrement : {c}");
+            Console.WriteLine($"c postincrement : {c++}");
+            Console.WriteLine($"c after postincrement : {c}");
+
+            c = 5;
+            Console.WriteLine($"c before preincrement : {c}");
+            Console.WriteLine($"c  preincrement : {++c}");
+            Console.WriteLine($"c after preincrement : {c}");
+
+           
+
+
+
+
+        }
+    }
+}
