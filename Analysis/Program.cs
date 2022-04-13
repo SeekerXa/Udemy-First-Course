eece888c6c1e08fa725f077e03737edb5a50bf3d
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-13 15:56:25 +0200

    statement analyze

---
+++ Analysis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Analysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passes = 0;
+            int failures = 0;
+            int studentCounter = 1;
+
+            while (studentCounter <= 10)
+            {
+                Console.Write("Enter result (1 = pass, 2 = fail): ");
+                int result = int.Parse(Console.ReadLine());
+
+                if (result == 1)
+                {
+                    passes++;
+                }
+                else
+                {
+                    failures++;
+                }
+                studentCounter++;
+            }
+
+            Console.WriteLine($"Passed: {passes}\nFailed: {failures}")  ;
+            if (passes > 8)
+            {
+                Console.WriteLine("Vonus to instructor!");
+
+            }
+
+        }                                                                          
+    }
+}
