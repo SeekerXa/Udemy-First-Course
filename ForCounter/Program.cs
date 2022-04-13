480cfd79b282e85a144490f8d1db10d341341b9f
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-13 16:35:26 +0200

    for

---
+++ ForCounter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write counter++");
+            for (int counter = 1;counter <=10;++counter)
+            {
+                Console.WriteLine(counter);
+            }
+
+            Console.WriteLine("write ++counter");
+            for (int counter = 1; counter <= 10; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+
+        }
+    }
+}
