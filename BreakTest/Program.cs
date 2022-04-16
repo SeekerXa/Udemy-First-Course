4ecd5893a950f6a4886d62c141ba750fd2c1ec1e
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-16 15:58:10 +0200

    Break and Continue
    Using Break and Continue in loops

---
+++ BreakTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            for (count = 1; count <= 10; count++)
+            {
+                if (count == 5)
+                {
+                    break;
+                }         
+                Console.WriteLine(count);
+
+            }
+            Console.WriteLine  ($"\n Broke out of loop at count = {count} ");
+
+            count = 1;
+            for (count = 1; count <= 10; count++)
+            {
+                if (count == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(count);
+
+            }
+            Console.WriteLine($"\n used continue to skip displaying 5 ");
+
+        }
+
+    }
+}
