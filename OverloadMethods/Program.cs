59b4ddd74a63d00dbcb1496e5f4b8b2963021e3c
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-27 13:50:40 +0200

    Method overloading

---
+++ OverloadMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Square of integer 7 is {Square(7)}");
+            Console.WriteLine($"Square of double 7.5 is {Square(7.5)}");
+        }
+
+            static int Square( int intValue)
+        {
+            return intValue * intValue;
+        }
+
+        static double Square(double intValue)
+        {
+            return intValue * intValue;
+        }
+
+      
+
+    }
+}
