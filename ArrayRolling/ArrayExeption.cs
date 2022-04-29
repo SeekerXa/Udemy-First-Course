75799b72823a5b7ad0b2b40b361be0516440f4bc
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-04-29 18:21:16 +0200

    Intro to Exception Handling
    Try and Catch

---
+++ ArrayRolling/ArrayExeption.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExeption
+{
+    internal class ArrayExeption
+    {
+        static void Main(string[] args)
+        {
+            int[] responses = { 1, 2, 3, 4, 5, 2, 3, 5, 1, 4, 3, 56, 2, 34, 5, 2, 3, 2, 3 };
+            var frequency = new int[6];
+
+            for(var answer = 0; answer < responses.Length; answer++)
+            {
+                try
+                {
+                    ++frequency[responses[answer]];
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                      Console.WriteLine(ex.Message);
+                    Console.WriteLine($"   responses[{answer}]={responses[answer]}\n");
+                }
+            }
+
+        
+
+            for(var rating = 1; rating < frequency.Length; rating++)    
+            {
+                
+                Console.WriteLine($"Frequency of responses {rating } is : {frequency[rating]}\n");
+
+            }
+             Console.WriteLine();
+        }
+    }
+}
