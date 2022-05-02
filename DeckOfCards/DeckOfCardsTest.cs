05e4856a502e0f5a2f9db79fd57caa3afd4b572c
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-02 12:50:38 +0200

    CardDeck

---
+++ DeckOfCards/DeckOfCardsTest.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    internal class DeckOfCardsTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(0 % 13);
+            Console.WriteLine(1 % 13);
+            Console.WriteLine(2 % 13);
+            Console.WriteLine(3 % 13);
+            Console.WriteLine(4 % 13);
+            Console.WriteLine(5 % 13);
+            Console.WriteLine(6 % 13);
+            Console.WriteLine(7 % 13);
+            Console.WriteLine(8 % 13);
+            Console.WriteLine(11 % 13);
+            Console.WriteLine(12 % 13);
+            Console.WriteLine(13 % 13);
+            Console.WriteLine(14 % 13);
+
+        }
+    }
+}
