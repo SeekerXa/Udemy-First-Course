466233565a89c043ed76a0c609074be0b6fbb357
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-06 11:37:46 +0200

    operator overloading

---
+++ ComplexTest/ComplexNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexTest
+{
+    public struct ComplexNumber
+    {
+        public double Real { get; }
+        public double Imaginary { get; }
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public override string ToString() => $"({Real} {(Imaginary < 0 ? "-" : "+")} {Math.Abs(Imaginary)}i)";
+
+        public static ComplexNumber operator +(ComplexNumber x, ComplexNumber y)
+        {
+            return new ComplexNumber(x.Real + y.Real, x.Imaginary + y.Imaginary);
+        }
+        public static ComplexNumber operator -(ComplexNumber x, ComplexNumber y)
+        {
+            return new ComplexNumber(x.Real - y.Real, x.Imaginary - y.Imaginary);
+        }
+        public static ComplexNumber operator *(ComplexNumber x, ComplexNumber y)
+        {
+            return new ComplexNumber(
+                x.Real * y.Real - x.Imaginary * y.Imaginary,
+                x.Real * y.Imaginary - y.Imaginary * x.Imaginary);
+        }
+    }
+}
---
+++ ComplexTest/ComplexTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexTest
+{
+    internal class ComplexTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a real part of complex x");
+            double realPart = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the imaginary part od complex number x:");
+            double imaginaryPart = double.Parse(Console.ReadLine());
+            var x = new ComplexNumber(realPart, imaginaryPart);
+
+            Console.WriteLine("Enter a real part of complex x");
+            realPart = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the imaginary part od complex number x:");
+            imaginaryPart = double.Parse(Console.ReadLine());
+            var y = new ComplexNumber(realPart, imaginaryPart);
+
+     
+            Console.WriteLine();
+            Console.WriteLine($"{x} + {y} = {x + y}");
+            Console.WriteLine($"{x} - {y} = {x - y}");
+            Console.WriteLine($"{x} * {y} = {x * y}");
+
+        }
+    }
+}
