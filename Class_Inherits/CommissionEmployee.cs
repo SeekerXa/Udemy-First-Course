1cdb695682b7661d34a34212d1fc4723b9e367f6
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-07 00:25:22 +0200

    01.  Inheritance

---
+++ Class_Inherits/CommissionEmployee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Inherits
+{
+    internal class CommissionEmployee : Object
+    {
+        public String FirstName { get; }
+        public String LastName { get; }
+        public string SocialSecurityNumber { get; }
+        private decimal grossSales;
+        private decimal commisionRate;
+
+        public CommissionEmployee(string firstName, string lastName, string sicialSecurityNumber, decimal grossSales, decimal commissionRate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = sicialSecurityNumber;
+            GrossSales = grossSales;
+            CommisionRate = commissionRate;
+        }
+
+        public decimal GrossSales
+        {
+            get { return grossSales; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value,
+                        $"{nameof(GrossSales)} must be >= 0");
+                }
+                grossSales = value;
+            }
+
+        }
+
+        public decimal CommisionRate
+        {
+            get { return commisionRate; }
+            set
+            {
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value,
+                        $"{nameof(CommisionRate)} must be > 0 and < 1 ");
+                }
+                commisionRate = value;
+            }
+        }
+
+        public decimal Earnings() => commisionRate * grossSales;
+        public override string ToString()
+        {
+            return $"Commission employee: {FirstName} {LastName}\n" +
+                $"Social security number: {SocialSecurityNumber}\n" +
+                $"gross Sales: {grossSales:C}\n" +
+                $"commision rate: {commisionRate:F2}";
+
+
+        }
+
+    }
+}
---
+++ Class_Inherits/CommissionEmployeeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Inherits
+{
+    internal class CommissionEmployeeTest
+    {
+
+        static void Main(string[] args)
+        {
+            var person1 = new CommissionEmployee("Patryk", "T....", "234243242", 5000, 0.01M);
+            Console.WriteLine(person1);
+        }
+    }
+}
