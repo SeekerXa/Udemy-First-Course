dc9461b408c41bbeb83aa0a78618fd48caa7c528
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-09 13:17:40 +0200

    Payroll System Using Polymorphism-Part 1
    Payroll System Using Polymorphism-Part 1

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Polymorphism
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+
+
+        public Employee(string fisrtName, string lastName, string socialSecurityNumber)
+        {
+            FirstName = fisrtName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"[{FirstName} {LastName}\n";
+        }
+
+        public abstract decimal Earnings();                                      
+
+    }
+}
